908e91b615478d17ad29c8981f8c1348dfd82360
KWW Code <amran@kismetworldwide.com>
2013-06-23 18:48:48 -0700

    send notifications

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Medlink::Application.routes.draw do
 devise_scope :admin_user do
   get "/requests/sendout" => "requests#sendout"
   get "/requests/sendoutemail" => "requests#sendoutemail"
+    get "/requests/notifications" => "requests#notifications"
+  get "/requests/sendoutnotifications" => "requests#sendoutnotifications"
   resources :requests 
   resources :patients
   resources :patients, :path => 'medlink' 
